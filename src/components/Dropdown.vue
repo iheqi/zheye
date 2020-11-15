9257e851c3a4227d076da4d693c988743aa69de6
何琦 <>
2020-11-15 21:31:27 +0800

    4-11

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -11,8 +11,9 @@
 
 <script lang="ts">
 import {
-  defineComponent, onMounted, onUnmounted, ref,
+  defineComponent, ref, watch,
 } from 'vue';
+import useClickOutside from '../hooks/useClickOutside';
 
 export default defineComponent({
   name: 'Dropdown',
@@ -30,20 +31,12 @@ export default defineComponent({
 
     const dropdownRef = ref < null | HTMLElement >(null);
 
-    const handler = (e: MouseEvent) => {
-      if (dropdownRef.value) {
-        if (!dropdownRef.value.contains(e.target as HTMLElement) && isOpen.value) {
-          isOpen.value = false;
-        }
+    const isClickOutside = useClickOutside(dropdownRef);
+
+    watch(isClickOutside, (nV) => {
+      if (isOpen.value && nV) {
+        isOpen.value = false;
       }
-    };
-
-    onMounted(() => {
-      document.addEventListener('click', handler);
-    });
-
-    onUnmounted(() => {
-      document.removeEventListener('click', handler);
     });
 
     return {
---
+++ src/hooks/useClickOutside.ts
@@ -0,0 +1,27 @@
+import {
+  onMounted, onUnmounted, Ref, ref,
+} from 'vue';
+
+export default (elementRef: Ref<null | HTMLElement>) => {
+  const isClickOutside = ref(false);
+
+  const handler = (e: MouseEvent) => {
+    if (elementRef.value) {
+      if (elementRef.value.contains(e.target as HTMLElement)) {
+        isClickOutside.value = false;
+      } else {
+        isClickOutside.value = true;
+      }
+    }
+  };
+
+  onMounted(() => {
+    document.addEventListener('click', handler);
+  });
+
+  onUnmounted(() => {
+    document.removeEventListener('click', handler);
+  });
+
+  return isClickOutside;
+};

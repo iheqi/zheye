f554c452359769904807b04a3b50e7ed4fa38005
何琦 <>
2020-11-15 20:06:20 +0800

    4-7

--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -6,14 +6,14 @@
       <li class="list-inline-item"><a href="#" class="btn btn-outline-light my-2">注册</a></li>
     </ul>
     <ul v-else class="list-inline mb-0">
-      <li class="list-inline-item"><a href="#"
-          class="btn btn-outline-light my-2">你好 {{user.name}}</a></li>
+      <dropdown :title="`你好，${user.name}`"></dropdown>
     </ul>
   </nav>
 </template>
 
 <script lang="ts">
 import { defineComponent, PropType } from 'vue';
+import Dropdown from './Dropdown.vue';
 
 export interface UserProps {
   isLogin: boolean;
@@ -27,6 +27,9 @@ export default defineComponent({
       type: Object as PropType<UserProps>,
       required: true,
     },
+  },
+  components: {
+    Dropdown,
   },
 });
 </script>

7b7efcfd0382bafd86e8b172ab845054f09aa5ae
qihe <>
2020-11-19 15:37:42 +0800

    5-2

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,27 @@
 <template>
   <div class="container">
     <global-header :user="user"></global-header>
-    <column-list :list="list"></column-list>
+    <!-- <column-list :list="list"></column-list> -->
+    <form action="">
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">邮箱地址</label>
+        <input
+          type="text" class="form-control" id="exampleInputEmail1"
+          v-model="emailRef.val"
+          @blur="validateEmail"
+        >
+        <div class="form-text" v-if="emailRef.error">{{emailRef.message}}</div>
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">密码</label>
+        <input type="password" class="form-control" id="exampleInputPassword1">
+      </div>
+    </form>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, reactive } from 'vue';
 import ColumnList, { ColumnProps } from './components/ColumnList.vue';
 import GlobalHeader, { UserProps } from './components/GlobalHeader.vue';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -50,6 +65,23 @@ export default defineComponent({
   },
 
   setup() {
+    const emailRef = reactive({
+      val: '',
+      error: false,
+      message: '',
+    });
+    const emailReg = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    const validateEmail = () => {
+      if (!emailRef.val.trim()) {
+        emailRef.error = true;
+        emailRef.message = 'can not be empty';
+      } else if (!emailReg.test(emailRef.val)) {
+        emailRef.error = true;
+        emailRef.message = 'should be valid email';
+      }
+    };
+
     return {
       list: testList,
       user: {
@@ -57,6 +89,8 @@ export default defineComponent({
         name: 'qihe',
         id: 0,
       },
+      emailRef,
+      validateEmail,
     };
   },
 });

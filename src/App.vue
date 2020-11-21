1810abee816c8e4080841ea2b9185bccbd7169bf
何琦 <>
2020-11-21 15:11:27 +0800

    6-4

--- src/App.vue
+++ src/App.vue
@@ -1,73 +1,30 @@
 <template>
   <div class="container">
     <global-header :user="user"></global-header>
-    <!-- <column-list :list="list"></column-list> -->
-    <validate-form @form-submit="onFormSubmit">
-      <div class="mb-3">
-        <label class="form-label">邮箱地址</label>
-        <validate-input
-          :rules="emailRules"
-          v-model="emailRef.val" placeholder="请输入"
-          ref="inputRef"
-        >
-        </validate-input>
-      </div>
-      {{emailRef.val}}
-
-      <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">密码</label>
-        <input type="password" class="form-control" id="exampleInputPassword1">
-      </div>
-    </validate-form>
+    <router-view></router-view>
+    <footer class="text-center py-4 text-secondary bg-light mt-6">
+      <small>
+        <ul class="list-inline mb-0">
+          <li class="list-inline-item">© 2020 者也专栏</li>
+          <li class="list-inline-item">课程</li>
+          <li class="list-inline-item">文档</li>
+          <li class="list-inline-item">联系</li>
+          <li class="list-inline-item">更多</li>
+        </ul>
+      </small>
+    </footer>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, reactive, ref } from 'vue';
-import ColumnList, { ColumnProps } from './components/ColumnList.vue';
 import GlobalHeader, { UserProps } from './components/GlobalHeader.vue';
-import ValidateInput, { RulesProp } from './components/ValidateInput.vue';
-import ValidateForm from './components/ValidateForm.vue';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-const testList: ColumnProps[] = [
-  {
-    id: 1,
-    title: 'test1的专栏',
-    description: '这是的test1专栏，有一段非常有意思的简介，可以更新一下欧',
-    avatar:
-      'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100',
-  },
-  {
-    id: 2,
-    title: 'test2的专栏',
-    description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧',
-    avatar:
-      'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100',
-  },
-  {
-    id: 3,
-    title: 'test3的专栏',
-    description: '这是的test1专栏，有一段非常有意思的简介，可以更新一下欧',
-    avatar:
-      'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100',
-  },
-  {
-    id: 4,
-    title: 'test4的专栏',
-    description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧',
-    avatar:
-      'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100',
-  },
-];
 
 export default defineComponent({
   name: 'App',
   components: {
-    ColumnList,
     GlobalHeader,
-    ValidateInput,
-    ValidateForm,
   },
 
   setup() {
@@ -77,7 +34,7 @@ export default defineComponent({
       error: false,
       message: '',
     });
-    const emailRules: RulesProp = [
+    const emailRules = [
       { type: 'required', message: '电子邮箱地址不能为空' },
       { type: 'email', message: '请输入正确的电子邮箱格式' },
     ];
@@ -88,7 +45,6 @@ export default defineComponent({
     };
 
     return {
-      list: testList,
       user: {
         isLogin: true,
         name: 'qihe',

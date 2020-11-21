38fcb0ae9afc940ed0bdceca43993cffdc45e2bf
何琦 <>
2020-11-21 18:24:17 +0800

    6-7

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import { createApp } from 'vue';
+import { createStore } from 'vuex';
 import App from './App.vue';
 import router from './router';
+
+const store = createStore({
+  state: {
+    count: 0,
+  },
+});
 
 const app = createApp(App);
 app.use(router);
